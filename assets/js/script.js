fd0d127fe757e9a75efc4b1126fecf442b06be96
sobagambyar-afk <sobagambyar@gmail.com>
2026-05-26 21:38:51 +0700

    Add filter product JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-// Custom JavaScript Think.ing
+$(document).ready(function () {
+
+  $(".filter-btn").on("click", function () {
+    $(".filter-btn").removeClass("active");
+    $(this).addClass("active");
+
+    const filterValue = $(this).data("filter");
+
+    if (filterValue === "all") {
+      $(".product-item").fadeIn();
+    } else {
+      $(".product-item").hide();
+      $('.product-item[data-category="' + filterValue + '"]').fadeIn();
+    }
+  });
+
+});
